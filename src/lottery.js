4634d651224ea3e1bb78cd48069e4954a9d349ae
gautham3195 <98706079+gautham3195@users.noreply.github.com>
2022-04-11 16:56:37 +0530

    Upload DeGamble

---
+++ src/lottery.js
@@ -0,0 +1,11 @@
+import web3 from './web3';
+
+//address to where the contract is deployed (Rinkeby blockchain)
+const address = '0x189C421405a526b40a8C074526efb18c93013573';
+
+//abi JSON after deployment
+const abi = [{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pickWinner","outputs":[]
+,"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getPlayers","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"enter","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"players","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}];
+
+
+export default new web3.eth.Contract(abi, address);
